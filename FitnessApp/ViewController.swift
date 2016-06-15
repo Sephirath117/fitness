e25232ccaca5e416a1840613c0a2a076560860d7
Karan Mehta <appllefanetickz@gmail.com>
2016-06-14 21:28:51 -0700

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Karan Mehta on 6/14/16.
+//  Copyright © 2016 Karan Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
